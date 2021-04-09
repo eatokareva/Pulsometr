61c7da7d7f0de1c0be512d051eb473780885a2b3
eatokareva <elena@tokareva.su>
2021-04-09 19:36:11 +0300

    modal_adapt

--- src/js/script.js
+++ src/js/script.js
@@ -101,4 +101,24 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+    // $('.button_shop').on('click', function() {
+    //     $('.overlay, #order').fadeIn('slow');
+
+    // });
+
+    $('.button_shop').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
 });
